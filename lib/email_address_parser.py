f94a68f9b9c03b074a1cfe7dfd03474e1bd85c0f
Teddy mwangi <teddy.maina@student.moringaschool.com>
2024-03-18 10:33:57 +0300

    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Using regular expression to split the string based on comma or space
+        addresses = re.split(r'[,\s]+', self.email_addresses)
+        # Filtering out empty strings and duplicates, then sorting
+        unique_addresses = sorted(set(filter(None, addresses)))
+        return unique_addresses
